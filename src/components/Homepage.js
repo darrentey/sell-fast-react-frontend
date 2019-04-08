2bea79a73724ec6a7f6800c12a751b7564223175
Darren Tey <darrentey19@gmail.com>
2019-04-08 22:48:17 +0800

    edit homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -44,44 +44,47 @@ class Homepage extends Component {
             localStorage.removeItem('reload_home');
             window.location.reload();
         }
-        let products = this.state.products.map((product) => {
+        if (this.state.products.length != 0){
+            let products = this.state.products.map((product) => {
 
-            if (product.user_id == localStorage.getItem('user_id')){
-                return (
-                    <Card key={product.id}>
-                        <CardBody>
-                        <CardTitle>{product.title}</CardTitle>
-                        <CardSubtitle>Product ID: {product.id}</CardSubtitle>
-                        </CardBody>
-                        {product.image_file}
-                        <CardBody>
-                        <CardText>{product.description}</CardText>
-                        <CardLink>{product.user_name}</CardLink>
-                        <CardLink>{product.date_posted}</CardLink>
-                        </CardBody>
-                        
-                        
-                        <button className="btn btn-danger" type="button" onClick={(e) => this.delete(e, product.id)}>Delete</button>
-                    </Card>
-                )
-            }else{
-                return (
-                    <Card key={product.id}>
-                        <CardBody>
-                        <CardTitle>{product.title}</CardTitle>
-                        <CardSubtitle>Product ID: {product.id}</CardSubtitle>
-                        </CardBody>
-                        {product.image_file}
-                        <CardBody>
-                        <CardText>{product.description}</CardText>
-                        <CardLink>{product.user_name}</CardLink>
-                        <CardLink>{product.date_posted}</CardLink>
-                        </CardBody>
-                    </Card>
-                )
-            }
-
-        });
+                if (product.user_id == localStorage.getItem('user_id')){
+                    return (
+                        <Card key={product.id}>
+                            <CardBody>
+                            <CardTitle>{product.title}</CardTitle>
+                            <CardSubtitle>Product ID: {product.id}</CardSubtitle>
+                            </CardBody>
+                            {product.image_file}
+                            <CardBody>
+                            <CardText>{product.description}</CardText>
+                            <CardLink>{product.user_name}</CardLink>
+                            <CardLink>{product.date_posted}</CardLink>
+                            </CardBody>
+                            
+                            
+                            <button className="btn btn-danger" type="button" onClick={(e) => this.delete(e, product.id)}>Delete</button>
+                        </Card>
+                    )
+                }else{
+                    return (
+                        <Card key={product.id}>
+                            <CardBody>
+                            <CardTitle>{product.title}</CardTitle>
+                            <CardSubtitle>Product ID: {product.id}</CardSubtitle>
+                            </CardBody>
+                            {product.image_file}
+                            <CardBody>
+                            <CardText>{product.description}</CardText>
+                            <CardLink>{product.user_name}</CardLink>
+                            <CardLink>{product.date_posted}</CardLink>
+                            </CardBody>
+                        </Card>
+                    )
+                }
+    
+            });
+        }
+        
         if (localStorage.getItem('user_id')){
             return (
                 <div className="container">
